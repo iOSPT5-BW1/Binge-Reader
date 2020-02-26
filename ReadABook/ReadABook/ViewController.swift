7560f657d24328074681f78a32f11ea2b0ae4363
Chris Price <christopher.r.price@gmail.com>
2020-02-25 22:29:06 -0500

    Added the updated ReadMe.

---
+++ ReadABook/ReadABook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadABook
+//
+//  Created by Chris Price on 2/25/20.
+//  Copyright © 2020 BuildWeek1x2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
