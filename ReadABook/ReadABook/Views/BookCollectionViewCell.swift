70f8fc35736d154f25f7d0c1201684ccf3bf2108
Chris Price <christopher.r.price@gmail.com>
2020-03-02 23:33:51 -0500

    Started coding the BookcollectionViewCell.

--- ReadABook/ReadABook/Views/BookCollectionViewCell.swift
+++ ReadABook/ReadABook/Views/BookCollectionViewCell.swift
@@ -6,4 +6,15 @@ class BookCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var bookTitleLabel: UILabel!
     @IBOutlet weak var bookImageView: UIImageView!
     
+    var book: Book? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    private func updateViews() {
+        guard let book = book else {return}
+        bookTitleLabel.text = book.title
+//        bookImageView.image =
+    }
 }
